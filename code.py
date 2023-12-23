6a7f4c4f974c155630e9d7a749f642401cadcf9d
MaryamSayed <132791426+MaryamSayedGitHub@users.noreply.github.com>
2023-12-23 22:31:36 +0200

    message

--- code.py
+++ code.py
@@ -1,2 +1,31 @@
-print("mervbnm,")
-print(2+986554)
+[33mcommit 315882504d7ca7cf7afd9a5c0e9bd97b85e0f602[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: MaryamSayed <132791426+MaryamSayedGitHub@users.noreply.github.com>
+Date:   Sat Dec 23 22:13:45 2023 +0200
+
+    change
+
+[1mdiff --git a/code.py b/code.py[m
+[1mindex 2754a7f..0ed152b 100644[m
+[1m--- a/code.py[m
+[1m+++ b/code.py[m
+[36m@@ -1 +1,2 @@[m
+[31m-print("mervbnm,")[m
+\ No newline at end of file[m
+[32m+[m[32mprint("mervbnm,")[m
+[32m+[m[32mprint(2+986554)[m
+\ No newline at end of file[m
+
+[33mcommit 9ccab7445a6db8d02f1898e4567bd7b7396688bf[m
+Author: MaryamSayed <132791426+MaryamSayedGitHub@users.noreply.github.com>
+Date:   Sat Dec 23 21:53:28 2023 +0200
+
+    merrrfghjnbvf1
+
+[1mdiff --git a/code.py b/code.py[m
+[1mnew file mode 100644[m
+[1mindex 0000000..2754a7f[m
+[1m--- /dev/null[m
+[1m+++ b/code.py[m
+[36m@@ -0,0 +1 @@[m
+[32m+[m[32mprint("mervbnm,")[m
+\ No newline at end of file[m
